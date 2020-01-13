e177ad58c9af87422e23995724b5c01e0cf2b52e
ListJ <619471333@qq.com>
2020-01-13 13:33:37 +0800

    publish compelete

---
+++ src/main/java/com/listj/community/community/model/Question.java
@@ -0,0 +1,16 @@
+package com.listj.community.community.model;
+
+import lombok.Data;
+
+@Data
+public class Question {
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Integer creator;
+    private Integer commentCount;
+    private Integer likeCount;
+}
