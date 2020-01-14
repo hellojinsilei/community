17d00beacad94bf53cda9d314888cfab2f88824e
ListJ <619471333@qq.com>
2020-01-14 10:04:42 +0800

    profilePage

--- src/main/java/com/listj/community/community/service/QuestionService.java
+++ src/main/java/com/listj/community/community/service/QuestionService.java
@@ -47,4 +47,30 @@ public class QuestionService {
         paginationDTO.setQuestions(questionDTOList);
         return paginationDTO;
     }
+
+    public PaginationDTO list(Integer userId, Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.countByUserId(userId);
+        paginationDTO.setPagination(totalCount,page,size);
+        //防止越界
+        if(page<1){
+            page=1;
+        }
+        if(page>paginationDTO.getTotalPage()){
+            page=paginationDTO.getTotalPage();
+        }
+        //计算偏移量size*(page-1)
+        Integer offset =size*(page-1);
+        List<Question> questions = questionMapper.listByUserId(userId,offset,size);
+        List<QuestionDTO> questionDTOList=new ArrayList<>();
+        for(Question question : questions){
+            User user=userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
+    }
 }
