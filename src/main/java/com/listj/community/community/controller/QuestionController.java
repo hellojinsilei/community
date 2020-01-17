eac662cfd6a52e2b06f19752f93e74fa47ea6c60
ListJ <619471333@qq.com>
2020-01-17 22:49:43 +0800

    相关问题功能完成

--- src/main/java/com/listj/community/community/controller/QuestionController.java
+++ src/main/java/com/listj/community/community/controller/QuestionController.java
@@ -31,12 +31,13 @@ public class QuestionController {
 //            throw new CustomizeException(CustomizeErrorCode.INVALID_INPUT);
 //        }
         QuestionDTO questionDTO=questionService.getById(id);
-
+        List<QuestionDTO> relatedQuestions=questionService.selectRelated(questionDTO);
         List<CommentDTO> comments = commentService.listByTargetId(id, CommentTypeEnum.QUESTION);
         //增加阅读数
         questionService.incView(id);
         model.addAttribute("question",questionDTO);
         model.addAttribute("comments",comments);
+        model.addAttribute("relatedQuestions",relatedQuestions);
         return "question";
     }
 }
